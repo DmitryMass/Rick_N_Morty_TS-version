ac16f13d1e33130b334b233b234e1f6e63f9bf87
Dmitry Moskalenko <dimamikron41@gmail.com>
2022-09-14 12:23:17 +0300

    added logo

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -6,19 +6,22 @@ import PageHome from '../Pages/PageHome';
 import PageCharacters from '../Pages/PageCharacters';
 import PageEpisodes from '../Pages/PageEpisodes';
 import PageCharacterItem from '../Pages/PageCharacterItem';
+import PageEpisodeItem from '../Pages/PageEpisodeItem';
 
 import '../Styles/reset.css';
 import '../Styles/vars.css';
 import styles from './app.module.css';
-import PageEpisodeItem from '../Pages/PageEpisodeItem';
+
+import logo from '../Assets/Images/logo.png';
 
 const App: FC = () => {
   return (
     <div className={styles.app}>
       <div className={styles.app__container}>
         <header className={styles.app__header}>
-          <Link className={styles.nav__link} to={ROUTE.HOME}>
-            Rk & Mty
+          <Link to={ROUTE.HOME}>
+            {/* Rk & Mty */}
+            <img className={styles.nav__logo} src={logo} alt='' />
           </Link>
           <nav className={styles.app__nav}>
             <Link className={styles.nav__link} to={ROUTE.CHARACTERS}>
